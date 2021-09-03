203f6ff1bbcd06e0128463a2b6f56f6c8e91a635
David <DEvangelista54@gmail.com>
2021-09-03 12:26:49 -0400

    ran out of time

--- index.js
+++ index.js
@@ -10,10 +10,41 @@ function editHeader(){
     document.getElementById("header").innerHTML = result;
 }
 editHeader()
-console.log(str)
+// console.log(str)
 
 /***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
+console.log("PLAYERS array looks like this:", PLAYERS)
+console.log("------")
+
+const playerContainer = document.querySelector(".player-container")
+
+PLAYERS.forEach(function(player){
+    renderPlayer(player) //renders player
+})
+
+function renderPlayer(player){
+    const playerCard = document.createElement('div')
+    playerCard.className = "player"
+    playerCard.dataset.number = player.number
+
+    const playerImg = document.createElement('img')
+    playerImg.src = player.photo
+    playerImg.alt = `${player.name}`
+
+    
+    const playerNick = `${player.nickname}`
+    playerNick.italics()
+    //test
+ 
+    const playerText = document.createElement('h3')
+    playerText.innerText = `${player.name} ${playerNick}`
+
+    playerCard.append(playerText, playerImg)
+    playerContainer.appendChild(playerCard)
+
+    // pokeCard.append(pokeImg, pokeName, pokeLikes, likesNum, likeBttn, deleteBttn);
+    // pokeContainer.appendChild(pokeCard);
+  }
 
 
 /***** Deliverable 4 *****/

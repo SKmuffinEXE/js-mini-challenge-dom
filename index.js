cf858698b98fb9b765e152d353787aab110632c8
Alexander Mandalar <alexandermandalar@gmail.com>
2021-09-03 11:26:06 -0400

    switch

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ console.log("Here's your header:", header)
 
 
 /***** Deliverable 2 *****/
-
+const str = header.textContent;
+function editHeader(){
+    const result = str.fontcolor("red");
+    document.getElementById("header").innerHTML = result;
+}
+editHeader()
+console.log(str)
 
 /***** Deliverable 3 *****/
 // console.log("PLAYERS array looks like this:", PLAYERS)
